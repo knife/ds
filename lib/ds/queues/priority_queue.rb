7bd07975cffbab1e2202fbf877084c79dd0749f7
Ben J. Woodcroft <donttrustben near gmail.com>
2014-01-08 09:22:39 +1000

    add ability to define definition of priority

--- lib/ds/queues/priority_queue.rb
+++ lib/ds/queues/priority_queue.rb
@@ -2,8 +2,31 @@ module DS
   class PriorityQueue < Queue
 
     #Create new priority queue. Internaly uses heap to store elements.
+    #
+    # The definition of priority can be defined in a block given to this constructor.
+    # Not providing a block is equivalent to providing the following block (higher
+    # numbers mean more priority)
+    #
+    #    DS::PriorityQueue.new{|a,b| a >= b}
+    #
+    # Alternately, to provide lower priority
+    #
+    #    DS::PriorityQueue.new{|a,b| a <= b}
+    #
+    # Note that the ordering must be consistent, otherwise strange things may happen.
+    # For instance the block should not define the following ordering:
+    #
+    #    rock > scissors
+    #    scissors > paper
+    #    paper > rock
     def initialize
-      @store = BinaryHeap.new {|parent,child| parent.key >= child.key}
+      if block_given?
+        @store = BinaryHeap.new do |parent,child|
+          yield parent.key, child.key
+        end
+      else
+        @store = BinaryHeap.new {|parent,child| parent.key >= child.key}
+      end
     end
 
 
@@ -13,10 +36,10 @@ module DS
       @store.insert(pair)
       self
     end
-    
+
     alias :push :enqueue
 
-    #Removes element with highest priority from queue . 
+    #Removes element with highest priority from queue .
     def dequeue
       if x = @store.shift
         x.value
