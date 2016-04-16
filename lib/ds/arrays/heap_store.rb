9fdda7c79b3c89bedbbafaea0c7623e4051b4399
knife <satre@o2.pl>
2016-04-16 22:46:10 +0200

    Refactor HeapStore

--- lib/ds/arrays/heap_store.rb
+++ lib/ds/arrays/heap_store.rb
@@ -1,30 +1,33 @@
 require 'forwardable'
 
 # Simple Array-like data structure with indexing starting from one.
-class HeapStore
-  extend Forwardable
 
-  def_delegators :@store, :push, :pop, :[], :[]=
+module DS
+  class HeapStore
+    extend Forwardable
 
-  def initialize(*args)
-    @store = [nil] + args.to_a
-  end
+    def_delegators :@store, :push, :pop, :[], :[]=
 
-  def length
-    @store.size - 1
-  end
+    def initialize(*args)
+      @store = [nil] + args.to_a
+    end
 
-  def first
-    @store[1]
-  end
+    def length
+      @store.size - 1
+    end
 
-  alias size length
+    def first
+      @store[1]
+    end
 
-  def empty?
-    length <= 0
-  end
+    alias size length
 
-  def to_a
-    @store[1..-1]
+    def empty?
+      length <= 0
+    end
+
+    def to_a
+      @store[1..-1]
+    end
   end
 end
